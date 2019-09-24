3650dedfc1edc8a0774cfe7a8cc6f2fbd8d9e515
Ubuntu <ubuntu@ip-172-31-95-142.ec2.internal>
2019-09-24 10:36:46 +0000

    Add tests for all of our coin denominations

--- vending_machine.py
+++ vending_machine.py
@@ -4,11 +4,17 @@ def get_change(amount):
     if amount == 0:  #make an if statement to make sure our first test does not break when doing the next test
         return []
                     
-    return [1]    #The if statement means that 0 results in no coins, and 1 makes our second test pass.
+    return [amount]  
         
  #remember to put our tests in here before the print("All tests pass!") and after our function
- 
-test_are_equal(get_change(0),[]) #we expect when we get zero change we get an empty list back
-test_are_equal(get_change((1),[1]) #we expect when we get one change that ge get 1 to be returned 
+ #write tests for out every coin denominations:
+test_are_equal(get_change(0), []) #we expect when we get zero change we get an empty list back
+test_are_equal(get_change(1), [1]) #we expect when we get one change that ge get 1 to be returned 
+test_are_equal(get_change(2), [2])
+test_are_equal(get_change(5), [5])
+test_are_equal(get_change(10), [10])
+test_are_equal(get_change(20), [20])
+test_are_equal(get_change(50), [50])
+test_are_equal(get_change(100), [100])
 
 print("All tests pass!")
