b7485692355c118b5eea4b7da04dac7426ae50f6
Ubuntu <ubuntu@ip-172-31-95-142.ec2.internal>
2019-09-24 10:30:46 +0000

    Add function, test and if statement to return 1 for any amount other than 0

--- vending_machine.py
+++ vending_machine.py
@@ -1,3 +1,14 @@
 from byotest import *
 
+def get_change(amount):
+    if amount == 0:  #make an if statement to make sure our first test does not break when doing the next test
+        return []
+                    
+    return [1]    #The if statement means that 0 results in no coins, and 1 makes our second test pass.
+        
+ #remember to put our tests in here before the print("All tests pass!") and after our function
+ 
+test_are_equal(get_change(0),[]) #we expect when we get zero change we get an empty list back
+test_are_equal(get_change((1),[1]) #we expect when we get one change that ge get 1 to be returned 
+
 print("All tests pass!")
