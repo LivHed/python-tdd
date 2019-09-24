f5fb7cd006e4230f070fa722b71d6277aea76a58
Ubuntu <ubuntu@ip-172-31-95-142.ec2.internal>
2019-09-24 11:19:51 +0000

    Refactor functions and add usd and eur coins

--- vending_machine.py
+++ vending_machine.py
@@ -1,15 +1,13 @@
 from byotest import *
 
-def get_change(amount):
-    if amount == 0:  #make an if statement to make sure our first test does not break when doing the next test
-        return []
-        
-    if amount in [100, 50, 20, 10, 5, 2, 1]:
-        return [amount]    
+usd_coins = [100, 50, 25, 10, 5, 2, 1]
+eur_coins = [100, 50, 20, 10, 5, 2, 1]
+
+def get_change(amount, coins=eur_coins):
      
-    change = [] 
-    for coin in [100, 50, 20, 10, 5, 2, 1]:
-        if coin <= amount:
+    change = []   #As long as the coin is less than or equal to the amount, it will carry on adding it.And only when it's not will it move on or return the change.
+    for coin in coins:
+        while coin <= amount:
             amount -= coin
             change.append(coin)
             
@@ -28,5 +26,8 @@ test_are_equal(get_change(50), [50])
 test_are_equal(get_change(100), [100])
 test_are_equal(get_change(3), [2, 1]) #we pass in the value of 3 and we expect 2 and 1 coming back as a change. 
 test_are_equal(get_change(7), [5, 2]) 
+test_are_equal(get_change(9), [5, 2, 2]) # pass in the value of 9, We'd expect to get back change of 5 cents, 2 cents, and 2 cents.
+test_are_equal(get_change(35, usd_coins), [25, 10]) #we´re passing in a value of 35 and a second argument(that currently don´t exist, and expect back 25 and 10.
+
 
 print("All tests pass!")
