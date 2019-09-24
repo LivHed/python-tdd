4b27b7542e0c5c8da44a15244d566bc0b060471d
Ubuntu <ubuntu@ip-172-31-95-142.ec2.internal>
2019-09-24 10:55:47 +0000

    Add change and if statement for change

--- vending_machine.py
+++ vending_machine.py
@@ -3,8 +3,18 @@ from byotest import *
 def get_change(amount):
     if amount == 0:  #make an if statement to make sure our first test does not break when doing the next test
         return []
-                    
-    return [amount]  
+        
+    if amount in [100, 50, 20, 10, 5, 2, 1]:
+        return [amount]    
+     
+    change = [] 
+    for coin in [100, 50, 20, 10, 5, 2, 1]:
+        if coin <= amount:
+            amount -= coin
+            change.append(coin)
+            
+    return change
+      
         
  #remember to put our tests in here before the print("All tests pass!") and after our function
  #write tests for out every coin denominations:
@@ -16,5 +26,7 @@ test_are_equal(get_change(10), [10])
 test_are_equal(get_change(20), [20])
 test_are_equal(get_change(50), [50])
 test_are_equal(get_change(100), [100])
+test_are_equal(get_change(3), [2, 1]) #we pass in the value of 3 and we expect 2 and 1 coming back as a change. 
+test_are_equal(get_change(7), [5, 2]) 
 
 print("All tests pass!")
